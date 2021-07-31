731eb0f1ed736cc1e566f2e87e405bfb93cf2558
Shubham Bhardwaj <shubham200015@yahoo.com>
2021-07-31 20:23:17 +0530

    Add product validation for fields and image size

--- Controllers/product.js
+++ Controllers/product.js
@@ -14,8 +14,19 @@ exports.create = (req, res) => {
                 error: "Image could not be uploaded"
             })
         }
+        const { name, description, price, category, quantity, shipping } = fields
+        if (!name || !description || !price || !category || !quantity || !shipping) {
+            return res.status(400).json({
+                error: "All fields are required"
+            })
+        }
         let product = new Product(fields)
         if (files.photo) {
+            if (files.photo.size > 1000000) {
+                return res.status(400).json({
+                    error: "Image should be less than 1Mb in size"
+                })
+            }
             product.photo.data = fs.readFileSync(files.photo.path)
             product.photo.contentType = files.photo.type
         }
