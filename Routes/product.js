f3b71d98ac71403d6c52848a8dc6c6134f21eb3b
Shubham Bhardwaj <shubham200015@yahoo.com>
2021-08-01 18:50:57 +0530

    Add product delete route

--- Routes/product.js
+++ Routes/product.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const { create, productById, read } = require('../Controllers/product');
+const { create, productById, read, remove } = require('../Controllers/product');
 const { requireSignin, isAuth, isAdmin } = require('../Controllers/auth');
 const { userById } = require('../Controllers/user');
 
 router.get('/product/:productId', read);
 router.post('/product/create/:userId', requireSignin, isAuth, isAdmin, create);
+router.delete('/product/:productId/:userId', requireSignin, isAuth, isAdmin, remove);
 
 router.param('userId', userById);
 router.param("productId", productById);
