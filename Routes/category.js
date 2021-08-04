eac93ac8a1e2d3579149c4df20ae5bf5656c6e7d
Shubham Bhardwaj <shubham200015@yahoo.com>
2021-08-04 20:38:27 +0530

    Add category read route

--- Routes/category.js
+++ Routes/category.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const { create } = require('../Controllers/category');
+const { create, categoryById, read } = require('../Controllers/category');
 const { requireSignin, isAuth, isAdmin } = require('../Controllers/auth');
 const { userById } = require('../Controllers/user');
 
+router.get('/category/:categoryId', read);
 router.post('/category/create/:userId', requireSignin, isAuth, isAdmin, create);
 
+router.param('categoryId', categoryById);
 router.param('userId', userById);
 
 module.exports = router;
