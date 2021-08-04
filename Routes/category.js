9337f9eb5eecb8a04e66917dcf697cc1bf6bf94f
Shubham Bhardwaj <shubham200015@yahoo.com>
2021-08-04 21:15:52 +0530

    Add category update, remove and list routes

--- Routes/category.js
+++ Routes/category.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-const { create, categoryById, read } = require('../Controllers/category');
+const { create, categoryById, read, update, remove, list } = require('../Controllers/category');
 const { requireSignin, isAuth, isAdmin } = require('../Controllers/auth');
 const { userById } = require('../Controllers/user');
 
 router.get('/category/:categoryId', read);
 router.post('/category/create/:userId', requireSignin, isAuth, isAdmin, create);
+router.put('/category/:categoryId/:userId', requireSignin, isAuth, isAdmin, update);
+router.delete('/category/:categoryId/:userId', requireSignin, isAuth, isAdmin, remove);
+router.get('/categories', list);
 
 router.param('categoryId', categoryById);
 router.param('userId', userById);
